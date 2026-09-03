012a081c14648309e798fd9d8c15a9ff57f9e65f
Calvin Bonner <me@calvinbonner.com>
2026-09-03 17:07:27 -0400

    Add initial implementation of index file with exports for core modules

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-console.log("Hello via Bun!");
+/** step 1 - determine if the repository is owned by "@minifw"
+ * by checking the package.json "name" to see if its prefixed with "@minifw"
+ */
+
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./copy-files/copy-files";
+export * from "./mini-tooling-error/mini-tooling-error";
+export * from "./sync/sync";
+export * from "./validate-gitignore/validate-gitignore";
+export * from "./validate-package/validate-package";
